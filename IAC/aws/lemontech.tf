9f4430514b9e47632653a3eafdd8e13a0d493a7f
Alexander Vilchez <alexander.vilchez@gdpteam.com>
2022-10-31 11:16:31 -0500

    Adding IAC to lemontehc

---
+++ IAC/aws/lemontech.tf
@@ -0,0 +1,15 @@
+locals {
+  env       = "prod"
+  project   = "lemontech"
+}
+
+module "lemontech-simple-app" {
+    #source          = "git@github.com:esleirter/IAC-modules.git//terraform/aws/vpc?ref=master"
+    source          = "../../../IAAC-modules/terraform/aws/lemontech-simple-app/"
+    env             = "${local.env}"
+    project         = "${local.project}"
+    k8s_version     = "1.23"
+    cidr_block_vpc  = "10.0.0.0/16"
+    cidr_block_pri  = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19"]
+    cidr_block_pub  = ["10.0.96.0/19", "10.0.128.0/19", "10.0.160.0/19"]
+}
---
+++ IAC/aws/provider.tf
@@ -0,0 +1,11 @@
+# Configure the AWS provider
+terraform {
+  backend "s3" {
+    bucket = "esleirter-lemon"
+    key    = "infrastructure/prod/lemontech/terraform.tfstate"
+    region = "us-east-2"
+  }
+  required_providers {
+    aws = ">= 4.37.0"
+  }
+}
